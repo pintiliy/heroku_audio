7b48ff0582846c3b0e499b550ebf8d7decad6c82
AnoNim <mrpintiliy@gmail.com>
2018-08-19 19:47:57 +0300

    asd

--- server.js
+++ server.js
@@ -31,6 +31,4 @@ app.get('/sound/:id', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT || 5000);
